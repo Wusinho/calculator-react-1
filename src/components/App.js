4cee9158495ba0e24c9e776531bb83c9a76a1c01
realisticattorney <g.axl.aquila@gmail.com>
2021-06-10 18:41:52 -0300

    Add short syntax to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,10 @@ import ButtonPanel from './ButtonPanel';
 
 const result = '0';
 const App = () => (
-  <div>
+  <>
     <Display result={result} />
     <ButtonPanel />
-  </div>
+  </>
 );
 
 export default App;
