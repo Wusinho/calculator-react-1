f14d02ce05f0a7342d3406179524a307cfab3d3c
realisticattorney <g.axl.aquila@gmail.com>
2021-06-10 01:23:23 -0300

    Add App.js

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
+
+const result = "0";
+const App = () => {
+  <div>
+    <Display result={result} />
+    <ButtonPanel />
+  </div>;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app";
+import PropTypes from "prop-types"; // ES6
+import Big from "big.js";
 
-const App = () => (
-  <div>
-    <h1>Calculator React</h1>
-  </div>
-);
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
