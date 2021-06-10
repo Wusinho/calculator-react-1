6e58248b58ca6045b6e766feea39f5993f617b3a
realisticattorney <g.axl.aquila@gmail.com>
2021-06-10 02:07:10 -0300

    Fix linters:

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import Button from './Button';
 
 const result = '0';
 const App = () => (
-  <>
+  <div>
     <Display result={result} />
     <ButtonPanel />
-    <Button name="" />
-  </>
+  </div>
 );
 
 export default App;
