046e66e58fc855cf5df79c78cc429712c8d42f85
realisticattorney <g.axl.aquila@gmail.com>
2021-06-09 23:42:21 -0300

    Deploy heroku

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import PropTypes from 'prop-types'; // ES6
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types"; // ES6
+import Big from "big.js";
+
+const App = () => (
+  <div>
+    <h1>Calculator React</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
