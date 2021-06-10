8d699a40b44210c5087edb92982a6e1c93c04429
realisticattorney <g.axl.aquila@gmail.com>
2021-06-10 18:47:00 -0300

    Remove needless var declaration

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import Display from './Display';
-import ButtonPanel from './ButtonPanel';
+import React from "react";
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
 
-const result = '0';
 const App = () => (
   <>
-    <Display result={result} />
+    <Display result={"0"} />
     <ButtonPanel />
   </>
 );
